0aeb04b9d52a8858de990189913554caaefe6c5a
Seunghyo Chun <seunghyo.chun@gmail.com>
2013-01-06 23:44:53 +0900

    got right answer

--- eulerian_tour.py
+++ eulerian_tour.py
@@ -31,14 +31,17 @@ def find_eulerian_tour(graph):
 		# if no odd degree -> make tour starting node_0
 		to_visit_set = set(graph)
 		allnode_list = list(allnode)
-		return get_path_for_nodes(graph, allnode_list[0], \
-				to_visit_set, [])
+		start = allnode_list[0]
+		return get_path_for_nodes(graph, start, \
+				to_visit_set, [start])
 	elif len(odd_node) == 2: # 2 odd degree node
+		print '### odd node = ', odd_node
 		# if 2 odd degree -> make tour starting one of odd degreed node
 		to_visit_set = set(graph)
 		allnode_list = list(allnode)
-		return get_path_for_nodes(graph, odd_node[0], \
-				to_visit_set, [])
+		start = odd_node[0]
+		return get_path_for_nodes(graph, start, \
+				to_visit_set, [start])
 	else:
 		# if >2 odd degree -> None
 		return None	
@@ -53,7 +56,8 @@ def get_path_for_nodes(graph, current, to_visit_set, path):
 			to_visit = ', to_visit_set, ', path =', path
 	if len(to_visit_set) == 0:
 		print 'finish path=', path
-		return path
+		if get_all_node(graph) == set(path):
+			return path
 
 
 
@@ -80,10 +84,11 @@ def get_path_for_nodes(graph, current, to_visit_set, path):
 			to_visit_set.difference(set([edge])), path + [peer])
 		print 'ret_a!! =', ret
 		if len(ret) > 0: # found
+			print 'found', 
 			a_path = ret
-		print 'ret_aa!! a_path =',a_path 
+			print 'ret_aa!! a_path =',a_path 
 
-		return a_path
+			return a_path
 
 	print 'ret_b!! =', ret
 	return a_path
@@ -162,6 +167,7 @@ def is_eulerian_tour(nodes, tour):
 
 def test():
 	#a_graph = [(1, 2), (2, 3), (3, 1)]
+	#a_graph = [(1, 2), (2, 3), (3, 1), (3, 4)]
 	a_graph = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9), (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 	#a_graph = [(1, 13), (1, 6), (6, 11), (3, 13), (8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9), (1, 12), (4, 12), (5, 14), (0, 1), (2, 3), (4, 11), (6, 9), (7, 14), (10, 13)]
 	#a_graph = [(8, 16), (8, 18), (16, 17), (18, 19), (3, 17), (13, 17), (5, 13),(3, 4), (0, 18), (3, 14), (11, 14), (1, 8), (1, 9), (4, 12), (2, 19),(1, 10), (7, 9), (13, 15), (6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
